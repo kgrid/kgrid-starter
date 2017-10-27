d38bd9686bc00c376909554bcdd34662c1d4f48f
pboisver <pboisver@umich.edu>
2017-10-27 11:34:23 -0400

    add example curl authenticated library load

--- init.sh
+++ init.sh
@@ -6,6 +6,10 @@ vagrant up
 
 # initialize the library with the objects from the shelf
 
-vagrant halt
+curl -XPOST http://localhost:8081/login -Fusername=admin@kgrid.org -Fpassword=admin123 --cookie-jar boo
+curl -XPUT "localhost:8081/knowledgeObject/99999-00000" -d "./activator/shelf/99999-fk4ff41k51.json" -H "Content-Type: application/json" --cookie boo
+# ...
 
 ./stop.sh
+vagrant halt
+
