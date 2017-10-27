96ecb7028adb5147cf7809807fb71c6d7c16c172
pboisver <pboisver@umich.edu>
2017-10-27 11:21:25 -0400

    add http-server version command

--- kgrid-download.sh
+++ kgrid-download.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+#########################################
+# Download tools needed to run Kgrid
+#########################################
+
 # Download jdk8, virtualbox, vagrant, node.js (plus npm). Then `npm install http-server`
 
 java -version
 vboxmanage -version
 vagrant --version
 node --version
+npm view http-server version
 
 # tell the user that they need to install jdk8, virtualbox, vagrant, node.js
 
-# curl  [kgrid library release] --output ./library
+
+#########################################
+# Download Kgrid components themselves
+#########################################
+
+# curl  [kgrid library release] --output ./library/library.war
+# curl  [kgrid library release] --output ./activator/activator.war
