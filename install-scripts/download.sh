d8d5b35d6456635e2f5c64916da67ee445509997
gqmeng <gqmeng.meng@gmail.com>
2017-10-30 16:11:41 -0400

    Update download.sh

--- install-scripts/download.sh
+++ install-scripts/download.sh
@@ -24,7 +24,7 @@ See the readme.html in this directory for details.
 # It needs to be initialized with 'vagrant up' which called from the init script
 
 
-curl -L https://github.com/kgrid/kgrid-library/releases/download/0.2.4-SNAPSHOT/{kgrid-library-0.2.4-SNAPSHOT.war} --output ../library/#1
+curl -L https://github.com/kgrid/kgrid-starter/releases/download/0.5/{kgrid-library-0.2.4-SNAPSHOT.war} --output ../library/#1
 curl -L https://github.com/kgrid/kgrid-activator/releases/download/kgrid-activator-0.5.8-SNAPSHOT/{activator-0.5.8-SNAPSHOT.war} --output ../activator/#1
 curl -L https://github.com/kgrid/python-adapter/releases/download/0.5.8-SNAPSHOT/{python-adapter-0.5.8-SNAPSHOT-jar-with-dependencies.jar} --output ../activator/adapters/#1
 
