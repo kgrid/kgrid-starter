0e975f63f56d5fa85c0d4f77d7eb210b24d0a19e
George Meng <gqmeng@umich.edu>
2017-10-31 15:45:15 -0400

    Updated download.bat

--- install-scripts/download.bat
+++ install-scripts/download.bat
@@ -25,7 +25,7 @@ See the readme.html in this directory for details.
 
 
 curl -L https://github.com/kgrid/kgrid-starter/releases/download/0.5/{kgrid-library-0.2.4-SNAPSHOT.war} --output ./library/#1
-curl -L https://github.com/kgrid/kgrid-activator/releases/download/kgrid-activator-0.5.8-SNAPSHOT/{activator-0.5.8-SNAPSHOT.war} --output ./activator/#1
+curl -L https://github.com/kgrid/kgrid-activator/releases/download/0.5.8-SNAPSHOT/{activator-0.5.8-SNAPSHOT.war} --output ./activator/#1
 curl -L https://github.com/kgrid/python-adapter/releases/download/0.5.8-SNAPSHOT/{python-adapter-0.5.8-SNAPSHOT-jar-with-dependencies.jar} --output ./activator/adapters/#1
 
 # The cancer-advisor client is preinstalled in the kgrid-starter
