1b5b6bf4cc32d58ec4435d65570d57d43b3fc9a6
pboisver <pboisver@umich.edu>
2017-10-31 15:27:52 -0400

    Update start and stop scripts to basically kill processes by port number Move http-server install to system section of readme

--- install-scripts/download.sh
+++ install-scripts/download.sh
@@ -23,9 +23,8 @@ See the readme.html in this directory for details.
 # The Fedora Commons repo (via fcrepo4-vagrant) is preinstalled in the kgrid-starter.
 # It needs to be initialized with 'vagrant up' which called from the init script
 
-
 curl -L https://github.com/kgrid/kgrid-starter/releases/download/0.5/{kgrid-library-0.2.4-SNAPSHOT.war} --output ./library/#1
-curl -L https://github.com/kgrid/kgrid-activator/releases/download/kgrid-activator-0.5.8-SNAPSHOT/{activator-0.5.8-SNAPSHOT.war} --output ./activator/#1
+curl -L https://github.com/kgrid/kgrid-activator/releases/download/0.5.8-SNAPSHOT/{activator-0.5.8-SNAPSHOT.war} --output ./activator/#1
 curl -L https://github.com/kgrid/python-adapter/releases/download/0.5.8-SNAPSHOT/{python-adapter-0.5.8-SNAPSHOT-jar-with-dependencies.jar} --output ./activator/adapters/#1
 
 # The cancer-advisor client is preinstalled in the kgrid-starter
