80d8ee5685d52213b92df04afb11593654135644
George Meng <gqmeng@umich.edu>
2017-10-31 21:51:29 -0400

    Update kg-start.bat and kg-stop.bat

--- kg-start.bat
+++ kg-start.bat
@@ -4,18 +4,18 @@ vagrant up
 cd..
 
 echo "Starting library"
-cd library
-start /b java -jar kgrid-library-0.2.4-SNAPSHOT.war --server.port=8081 &
-cd..
-timeout /t 20
-cls
+::cd library
+start /b java -jar library\kgrid-library-0.2.4-SNAPSHOT.war --server.port=8081 &
+::cd..
+::timeout /t 20
+::cls
 
 echo "Starting activator"
-cd activator
-start /b  java -jar activator-0.5.8-SNAPSHOT.war --server.port=8082 --activator.home=activator --library.url=http://localhost:8081 &
-cd..
-timeout /t 15
+::cd activator
+start /b  java -jar activator\activator-0.5.8-SNAPSHOT.war --server.port=8082 --activator.home=activator --library.url=http://localhost:8081 &
+::cd..
+::timeout /t 15
 
 echo "Starting cancer advisor"
-cd cancer-advisor
+::cd cancer-advisor
 start /b "Kgrid" http-server -p 8083 -o
--- kg-stop.bat
+++ kg-stop.bat
@@ -1,5 +1,10 @@
+echo "Stopping http-server"
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8083') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
+echo "Stopping KGRID Activator"
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8082') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
+echo "Stopping KGRID Library"
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8081') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
 cd fcrepo4-vagrant-4.5.1
 echo "Stopping vagrant"
 vagrant halt
 cd ..
-taskkill /f /fi "imagename eq cmd.exe" /fi "imagename eq node.exe"
