38bdf01a1388b9388b9a7107dfdf5fd653521931
pboisver <pboisver@umich.edu>
2017-11-01 10:40:01 -0400

    update init script to use activator shelf; delete library specific shelf_0; logout script after init

--- install-scripts/download.sh
+++ install-scripts/download.sh
@@ -23,8 +23,18 @@ See the readme.html in this directory for details.
 # The Fedora Commons repo (via fcrepo4-vagrant) is preinstalled in the kgrid-starter.
 # It needs to be initialized with 'vagrant up' which called from the init script
 
+echo "Downloading the Library (kgrid-library-0.2.4-SNAPSHOT.war)"
 curl -L https://github.com/kgrid/kgrid-starter/releases/download/0.5/{kgrid-library-0.2.4-SNAPSHOT.war} --output ./library/#1
+
+echo "Downloading the Activator (activator-0.5.8-SNAPSHOT.war)"
 curl -L https://github.com/kgrid/kgrid-activator/releases/download/0.5.8-SNAPSHOT/{activator-0.5.8-SNAPSHOT.war} --output ./activator/#1
+
+echo "Downloading the Python adapter (python-adapter-0.5.8-SNAPSHOT-jar-with-dependencies.jar)"
 curl -L https://github.com/kgrid/python-adapter/releases/download/0.5.8-SNAPSHOT/{python-adapter-0.5.8-SNAPSHOT-jar-with-dependencies.jar} --output ./activator/adapters/#1
+
+echo "
+Finished downloading.... If there were errors see the setup guide,
+check that you are connected to the internet, and try again.
+"
 
 # The cancer-advisor client is preinstalled in the kgrid-starter
--- install-scripts/init.bat
+++ install-scripts/init.bat
@@ -1,7 +1,10 @@
-curl --request POST http://localhost:8081/login -Fusername=admin@kgrid.org -Fpassword=admin123 --cookie-jar store
-curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4jh3tk9s" -d "@./library/shelf_0/99999-fk4jh3tk9s" -H "Content-Type: application/json" --cookie store
-curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4n87hh26" -d "@./library/shelf_0/99999-fk4n87hh26" -H "Content-Type: application/json" --cookie store
-curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk41g0zc28" -d "@./library/shelf_0/99999-fk41g0zc28" -H "Content-Type: application/json" --cookie store
-curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4474n87d" -d "@./library/shelf_0/99999-fk4474n87d" -H "Content-Type: application/json" --cookie store
-curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4571p57h" -d "@./library/shelf_0/99999-fk4571p57h" -H "Content-Type: application/json" --cookie store
-curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4805c32z" -d "@./library/shelf_0/99999-fk4805c32z" -H "Content-Type: application/json" --cookie store
+REM initialize the library with the objects from the shelf
+
+curl --request POST "http://localhost:8081/login" -Fusername=admin@kgrid.org -Fpassword=admin123 --cookie-jar store
+curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4jh3tk9s" -d "@./activator/shelf/99999-fk4jh3tk9s" -H "Content-Type: application/json" --cookie store
+curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4n87hh26" -d "@./activator/shelf/99999-fk4n87hh26" -H "Content-Type: application/json" --cookie store
+curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk41g0zc28" -d "@./activator/shelf/99999-fk41g0zc28" -H "Content-Type: application/json" --cookie store
+curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4474n87d" -d "@./activator/shelf/99999-fk4474n87d" -H "Content-Type: application/json" --cookie store
+curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4571p57h" -d "@./activator/shelf/99999-fk4571p57h" -H "Content-Type: application/json" --cookie store
+curl --request PUT "http://localhost:8081/knowledgeObject/99999-fk4805c32z" -d "@./activator/shelf/99999-fk4805c32z" -H "Content-Type: application/json" --cookie store
+curl --request POST "http://localhost:8081/logout" --cookie store
