197698873d4a209ca21311ac354925f9d7317fd8
George Meng <gqmeng@umich.edu>
2017-11-06 12:54:43 -0500

    Changed back to 808x

--- kg-start.bat
+++ kg-start.bat
@@ -5,12 +5,12 @@ vagrant up
 cd..
 
 echo "Starting library"
-start /min java -jar library/kgrid-library-0.2.4-SNAPSHOT.war --server.port=48081 --ezid.mock=true --fcrepo.port=48080
+start /min java -jar library/kgrid-library-0.2.4-SNAPSHOT.war --server.port=8081 --ezid.mock=true --fcrepo.port=8080
 
 echo "Starting activator"
-start /min  java -jar activator/activator-0.5.8-SNAPSHOT.war --server.port=48082 --activator.home=activator --library.url=http://localhost:48081
+start /min  java -jar activator/activator-0.5.8-SNAPSHOT.war --server.port=8082 --activator.home=activator --library.url=http://localhost:8081
 
 echo "Starting web server"
 echo "Welcome Page will be opened in your default browser."
 echo "It may take a while for the Library and Activator to start and be ready."
-start /min "Kgrid" live-server --port=48083 --host=localhost
+start /min "Kgrid" live-server --port=8083 --host=localhost
